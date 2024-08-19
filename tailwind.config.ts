826cbb22c3aa9cc509e8c3fb54c4ab3ec77f731f
Hamdani <hamdani@synergizetech.dev>
2024-08-19 11:05:17 +0800

    setup project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,18 @@
-import type { Config } from "tailwindcss";
+import forms from '@tailwindcss/forms'
+import lineClamp from '@tailwindcss/line-clamp'
+import typography from '@tailwindcss/typography'
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './features/**/*.{js,ts,jsx,tsx,mdx}',
+    './layouts/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    extend: {},
   },
-  plugins: [],
-};
-export default config;
+  plugins: [forms, lineClamp, typography],
+}
+export default config
